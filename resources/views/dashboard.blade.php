1efe4436a3c084a3e7d523340bad7819a01065e1
Nipun Lakshan <nipunlakshankumara@gmail.com>
2025-09-24 01:44:34 +0530

    feat(ui/ux): expand and collapse create task form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,34 @@
 <x-layouts.app :title="__('Dashboard')">
-    <div class="relative flex h-full max-h-[92vh] w-full flex-col gap-4 rounded-xl">
+    <div
+        x-data="{ open: false }"
+        class="relative flex h-full max-h-[92vh] w-full flex-col gap-4 rounded-xl">
         <div
-            class="overflow-hidden min-h-[200px] overflow-y-auto rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <h2 class="px-4 my-4 text-xl font-semibold">Create Task</h2>
-            <livewire:tasks.create />
+            x-bind:class="{ 'min-h-[200px]': open, '': !open }"
+            class="overflow-hidden overflow-y-auto rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <div class="flex justify-between border-b border-neutral-200 dark:border-neutral-700">
+                <h2 class="my-4 px-4 text-xl font-semibold">Create Task</h2>
+                <flux:button
+                    icon="chevron-down"
+                    variant="ghost"
+                    class="m-4 p-2"
+                    x-bind:class="{ 'rotate-180': open, 'rotate-0': !open }"
+                    aria-haspopup="true"
+                    aria-controls="create-task-form"
+                    aria-expanded="open ? 'true' : 'false'"
+                    aria-label="open ? 'Close create task form' : 'Open create task form'"
+                    id="create-task-button"
+                    x-on:click="open = !open"
+                    x-bind:aria-expanded="open.toString()"
+                    x-bind:aria-label="open ? 'Close create task form' : 'Open create task form'" />
+            </div>
+            <div x-show="open" id="create-task-form" aria-labelledby="create-task-button">
+                <livewire:tasks.create />
+            </div>
         </div>
 
         <div
-            class="flex flex-col flex-1 max-h-[600px] min-h-[300px]  gap-2 w-full overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <h4 class="px-4 my-4 text-xl font-semibold">My Tasks</h4>
+            class="flex min-h-[300px] w-full flex-1 flex-col gap-2 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <h4 class="my-4 px-4 text-xl font-semibold">My Tasks</h4>
             <div
                 class="flex h-full flex-col overflow-hidden border-t border-neutral-200 dark:border-neutral-700">
                 <livewire:tasks.list />
@@ -20,7 +40,7 @@
         @livewire('tasks.delete')
 
         @if (session('status'))
-            <div class="fixed mx-auto left-[70%] bottom-4 right-4 z-50">
+            <div class="fixed bottom-4 left-[70%] right-4 z-50 mx-auto">
                 <flux:callout variant="success" icon="check-circle" heading="Success">
                     {{ session('status') }}
                 </flux:callout>
